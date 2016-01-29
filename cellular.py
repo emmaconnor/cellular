d6d2e4e87d279488b088aeecdaa99f5bfa31e95d
Joseph Connor <josephc346@gmail.com>
2016-01-29 08:40:14 -0500

    Add probabilistic measures, and redo saving code, plus other small tweaks

--- cellular.py
+++ cellular.py
@@ -1,5 +1,8 @@
+import collections
+import itertools
 import random
 
+from fractions import Fraction
 from PIL import Image, ImageDraw
 
 
@@ -52,18 +55,18 @@ class TotalisticCellularAutomaton:
                 return 0
 
         cache = {}
-        def C(k):
-            if k in cache: return cache[k]
-            if N == 1:
+        def D(k, n):
+            if (k, n) in cache: return cache[k, n]
+            if n == 1:
                 result = 1 if k < K else 0
             elif k < K:
-                result = choose(N + k - 1, k)
+                result = choose(n + k - 1, k)
             else:
-                result = sum(C(k - j, N - 1) for j in range(K))
-            cache[k] = result
+                result = sum(D(k - j, n - 1) for j in range(K))
+            cache[k, n] = result
             return result
 
-        n0 = sum(C(i) for i in range(len(self.rules)) if self.rules[i] == 0)
+        n0 = sum(D(N, i) for i in range(len(self.rules)) if self.rules[i] == 0)
         T = K**N
             
         return 1 - n0/T
@@ -79,6 +82,46 @@ class TotalisticCellularAutomaton:
     @property
     def entropy_t(self):
         return 0.0
+
+    def get_probs(self):
+        print("Modeled: ")
+        N = self.radius*2 + 1
+
+        probs = [Fraction(1, self.n_states) for _ in range(self.n_states)]
+
+        def product(nums):
+            r = 1
+            for n in nums:
+                r *= n
+            return r
+
+        for x in range(5):
+            print(format_floats([float(p) for p in probs]))
+            new_probs = [0 for _ in probs]
+            for neighborhood in itertools.product(*[range(self.n_states) for _ in range(N)]):
+                p_n = product(probs[state] for state in neighborhood)
+                new_state = self.rules[sum(neighborhood)]
+                new_probs[new_state] += p_n
+            probs = new_probs
+
+        return probs
+
+    def __str__(self):
+        return ''.join(str(r) for r in self.rules)
+
+def format_floats(floats):
+    fstr = '  '.join('{:10.08f}' for _ in floats)
+    return fstr.format(*floats)
+
+def get_real_probs(ca, history):
+    total = len(history) * len(history[0])
+
+    c = collections.Counter()
+    for row in history:
+        c.update(row)
+    probs = [c[state]/total for state in range(ca.n_states)]
+    return probs
+    
 
         
 def draw_history(ca, history):
@@ -97,27 +140,37 @@ def draw_history(ca, history):
 
 
 def main():
+    cas_folder = 'cas/'
+
     ca = TotalisticCellularAutomaton()
 
-    while any(rule != 0 for rule in ca.rules):
+    base = str(ca)
 
+    while any(rule != 0 for rule in ca.rules):
         ca.reseed()
         history = [ca.cells]
-        for x in range(1000):
+
+        for x in range(800):
             ca.next_gen()
             history.append(ca.cells)
             if all(cell == 0 for cell in ca.cells):
                 break
 
         image = draw_history(ca, history)
-        image.save(''.join(str(r) for r in ca.rules) + '.png')
         image.show()
 
         print('[' + ' '.join(str(r) for r in ca.rules) + ']')
         print(("{:10s}    " * 4).format('lambda', 'lambda_t', 'entropy', 'entropy_t'))
         print(("{:10.8f}    " * 4).format(ca.lam, ca.lam_t, ca.entropy, ca.entropy_t))
-        ca_class = input("Class: ")
-        observations = input("Observations: ")
+        ca.get_probs()
+        print("Actual: ")
+        print(format_floats(get_real_probs(ca, history)))
+        classification = None
+
+        while classification not in ('1', '2', '3', '4'):
+            classification = input("Class: ")
+
+        image.save(cas_folder + '{}-{}-{}.png'.format(base, ca, classification))
 
         ca.decimate()
 
